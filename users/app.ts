fc12442acbd4420f822c827367459082ba16b29c
Dadit <dadit.utama@gmail.com>
2024-08-14 10:47:46 +0700

    fix: setup PORT as env

--- users/app.ts
+++ users/app.ts
@@ -1,14 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import { userRouter } from "./routes/user.route";
+import { connectDb } from "./db-connect/db.connect";
 
 dotenv.config();
+connectDb;
 
 const app = express();
-const port: number = 8080;
 app.use(express.json());
 app.use("/api/v1/users", userRouter);
 
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
+app.listen(process.env.PORT, () => {
+  console.log(`App listening on port ${process.env.PORT}`);
 });
