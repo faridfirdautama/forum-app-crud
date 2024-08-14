0b9035c3096abf4b116137e0884e63d39566227b
Dadit <dadit.utama@gmail.com>
2024-08-14 10:32:16 +0700

    fix: delete unnecessary services

--- users/app.ts
+++ users/app.ts
@@ -2,8 +2,6 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import { userRouter } from "./routes/user.route";
-import { threadRouter } from "./routes/thread.route";
-import { repliesRouter } from "./routes/replies.route";
 
 dotenv.config();
 
@@ -11,8 +9,6 @@ const app = express();
 const port: number = 8080;
 app.use(express.json());
 app.use("/api/v1/users", userRouter);
-app.use("/api/v1/threads", threadRouter);
-app.use("/api/v1/replies", repliesRouter);
 
 mongoose
   .connect(process.env.URI_MONGO_DEVSCALE as string)
--- users/routes/replies.route.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import RepliesController from "../controllers/replies.controller";
-
-export const repliesRouter = express.Router();
-
-repliesRouter.get("/", RepliesController.handleGetAllReplies);
-repliesRouter.post("/", RepliesController.handleCreateReplies);
-repliesRouter.patch("/:id", RepliesController.handleUpdateReplies);
-repliesRouter.delete("/:id", RepliesController.handleDeleteReplies);
